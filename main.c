be418f26df3901efb99b29e77abf034d557c66d9
bob <bob@edurea.co>
2020-12-14 11:58:16 +0530

    added printMessage() function

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+void printMessage(void);
 int main(void){
 	printf("Hello World\n");
 	return EXIT_SUCCESS;
+}
+void printMessage(void){
+	printf("* Hello World\n");
 }
 
